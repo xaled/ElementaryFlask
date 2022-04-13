cb0c512bc4ff7f2b387a72b54581da0696a6be72
Khalid Grandi <kh.grandi@gmail.com>
2022-04-13 19:41:21 +0100

    mongoengine listing + listing fixes

--- src/elementary_flask/components/weak/listing/__init__.py
+++ src/elementary_flask/components/weak/listing/__init__.py
@@ -4,5 +4,9 @@ from .filter import *
 from .listing import *
 from .renderer import *
 from .sort import *
+from .mongo import *
 
-__all__ = action.__all__ + column.__all__ + filter.__all__ + listing.__all__ + renderer.__all__ + sort.__all__
+__all__ = (
+        action.__all__ + column.__all__ + filter.__all__ + listing.__all__ + renderer.__all__ + sort.__all__
+        + mongo.__all__
+)
