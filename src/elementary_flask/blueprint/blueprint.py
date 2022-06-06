c310e8716fd989c2cdb84d22eca763f90238527a
Khalid Grandi <kh.grandi@gmail.com>
2022-06-06 21:20:42 +0100

    blueprint navigation group + fixes

--- src/elementary_flask/blueprint/blueprint.py
+++ src/elementary_flask/blueprint/blueprint.py
@@ -2,13 +2,16 @@ __all__ = ["ElementaryBluePrint"]
 
 from flask import Blueprint, Flask
 
+from elementary_flask.components import NavigationGroup, get_icon
 from .scaffold import ElementaryScaffold
 
 
 class ElementaryBluePrint(Blueprint, ElementaryScaffold):
-    def __init__(self, name, *args, **kwargs):
-        Blueprint.__init__(self, name, *args, **kwargs)
+    def __init__(self, name, import_name, navigation_title=None, navigation_icon=None, *args, **kwargs):
+        Blueprint.__init__(self, name, import_name, *args, **kwargs)
         ElementaryScaffold.__init__(self)
+        self.navigation_title = navigation_title or name
+        self.navigation_icon = get_icon(navigation_icon)
 
     def register(self, app, options: dict) -> None:
         from elementary_flask.app import ElementaryFlask
@@ -23,7 +26,7 @@ class ElementaryBluePrint(Blueprint, ElementaryScaffold):
         else:
             raise ValueError('Unknown app type')
 
-        if flask_app:
+        if not flask_app:
             raise ValueError('Flask App is not initialized yet. None value provided')
 
         self.elementary_bp_register(elementary_app)
@@ -31,4 +34,8 @@ class ElementaryBluePrint(Blueprint, ElementaryScaffold):
 
     def elementary_bp_register(self, elementary_app):
         elementary_app.crontab.extend(self.elementary_ns.crontab)
-        elementary_app.extend_navigation_map(self.elementary_ns.navigation_map)
+        elementary_app.extend_navigation_map(
+            NavigationGroup(self.navigation_title,
+                            icon=self.navigation_icon,
+                            items_list=self.elementary_ns.navigation_map)
+        )
--- src/elementary_flask/utils/j2_isinstance.py
+++ src/elementary_flask/utils/j2_isinstance.py
@@ -1,20 +1,35 @@
-_CLS_MAPPING = {
-    # Elementary classes
+_CLS_MAPPING = dict()
 
-    # Built-in Types
-    'str': str,
-    'bytes': bytes,
-    'int': int,
-    'float': float,
-    'complex': complex,
-    'bool': bool,
-    'list': list,
-    'tuple': tuple,
-    'range': range,
-    'dict': dict,
-    'set': set,
-}
+
+def _get_cls_mapping():
+    if len(_CLS_MAPPING) == 0:
+        # init
+        from elementary_flask.components import NavigationGroup, NavigationLink, NavigationItem, NavigationSeparator
+        _CLS_MAPPING.update(
+            {
+                # Elementary classes - Navigation
+                'NavigationGroup': NavigationGroup,
+                'NavigationLink': NavigationLink,
+                'NavigationItem': NavigationItem,
+                'NavigationSeparator': NavigationSeparator,
+
+                # Built-in Types
+                'str': str,
+                'bytes': bytes,
+                'int': int,
+                'float': float,
+                'complex': complex,
+                'bool': bool,
+                'list': list,
+                'tuple': tuple,
+                'range': range,
+                'dict': dict,
+                'set': set,
+            }
+        )
+
+    return _CLS_MAPPING
 
 
 def j2_isinstance(obj, cls_str):
-    return isinstance(obj, _CLS_MAPPING.get(cls_str))
+    return isinstance(obj, _get_cls_mapping().get(cls_str))
