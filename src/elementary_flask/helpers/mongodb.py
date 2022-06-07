d134fb0ea5bed61cd3560fab6d12a6c8ff4b3169
Khalid Grandi <kh.grandi@gmail.com>
2022-06-08 00:15:52 +0100

    mongodb & redis config fixes

--- src/elementary_flask/helpers/mongodb.py
+++ src/elementary_flask/helpers/mongodb.py
@@ -1,4 +1,4 @@
-__all__ = ['mongo_client', 'mongo_db', 'connect_mongoengine']
+__all__ = ['mongo_client', 'mongo_db', 'connect_mongoengine', 'get_mongo_config']
 
 # from flask import current_app, _app_ctx_stack  # noqa
 # MONGO_CLIENT_ATTRIBUTE_NAME = 'elementary_flask_mongo_client'
@@ -17,18 +17,18 @@ _mongo_client = None
 #         return getattr(ctx, MONGO_CLIENT_ATTRIBUTE_NAME)
 
 
-def mongo_client(config=None):
+def mongo_client():
     global _mongo_client
     if _mongo_client is None:
-        _mongo_client = _get_mongo_client(config=config)
+        _mongo_client = _get_mongo_client()
     return _mongo_client
 
 
-def mongo_db(db, config=None):
-    return mongo_client(config=config)[db]
+def mongo_db(db):
+    return mongo_client()[db]
 
 
-def _setup_mongo_config(db=None, config=None):
+def get_mongo_config(config=None):
     app_config = config or get_helper_config()
     elementary_config = app_config.setdefault('ELEMENTARY_FLASK', dict())
     mongo_config = elementary_config.setdefault('mongodb', False)
@@ -42,31 +42,32 @@ def _setup_mongo_config(db=None, config=None):
         if 'url' not in mongo_config:
             host = mongo_config.setdefault('host', 'localhost' if debug else 'mongo')
             port = mongo_config.setdefault('port', 27017)
-            db = db or mongo_config.setdefault(
-                'db',
-                app_config.setdefault('ELEMENTARY_FLASK_APP_NAME', 'test_app').lower()
-            )
-            mongo_config.setdefault('url', "mongodb://%s:%d/%s" % (host, port, db))
+            # db = db or mongo_config.setdefault(
+            #     'db',
+            #     app_config.setdefault('ELEMENTARY_FLASK_APP_NAME', 'test_app').lower()
+            # )
+            # mongo_config.setdefault('url', "mongodb://%s:%d/%s" % (host, port, db))
+            mongo_config.setdefault('url', "mongodb://%s:%d/" % (host, port))
 
         # mongo_config.setdefault('connect_mongoengine', False)
 
     return mongo_config
 
 
-def _get_mongo_client(config=None):
-    mongo_config = _setup_mongo_config(config=config)
+def _get_mongo_client():
+    mongo_config = get_mongo_config()
     if mongo_config:
         import pymongo  # noqa
         return pymongo.MongoClient(mongo_config['url'])
 
 
-def connect_mongoengine(db=None, alias=None, config=None, **kwargs):
-    mongo_config = _setup_mongo_config(db=db, config=config)
+def connect_mongoengine(db=None, alias=None, **kwargs):
+    mongo_config = get_mongo_config()
     if mongo_config and mongo_config.get('connect_mongoengine', False):
         import mongoengine  # noqa
         if alias is None:
-            mongoengine.connect(host=mongo_config['url'], **kwargs)
-        return mongoengine.connect(host=mongo_config['url'], alias=alias, **kwargs)
+            mongoengine.connect(host=mongo_config['url'] + db, **kwargs)
+        return mongoengine.connect(host=mongo_config['url'] + db, alias=alias, **kwargs)
 
 # def _teardown(exception):
 #     ctx = _app_ctx_stack.top
--- src/elementary_flask/helpers/redis.py
+++ src/elementary_flask/helpers/redis.py
@@ -1,4 +1,4 @@
-__all__ = ['redis_client', 'pottery_factory']
+__all__ = ['redis_client', 'pottery_factory', 'get_redis_config']
 
 # from flask import current_app, _app_ctx_stack  # noqa
 from redis import Redis, ConnectionPool
@@ -29,28 +29,31 @@ _redis_pools = dict()
 #             current_elementary_flask_app.append_teardown(_teardown(attribute_name))
 #         return getattr(ctx, attribute_name)
 
-def redis_client(db=None, config=None):
+def redis_client(db=None):
     global _redis_clients
     if db not in _redis_clients or _redis_clients[db] is None:
-        _redis_clients[db] = _get_redis_client(db, config=config)
+        _redis_clients[db] = _get_redis_client(db)
     return _redis_clients[db]
 
 
-def _get_redis_client(db=None, config=None):
+def _get_redis_client(db=None):
     global _redis_pools
     if db not in _redis_pools or _redis_pools[db] is None:
-        _redis_pools[db] = _get_redis_pool(db, config=config)
+        _redis_pools[db] = _get_redis_pool(db)
 
     if _redis_pools[db] is None:
         return None
     return Redis(connection_pool=_redis_pools[db])
 
 
-def _get_redis_pool(db=None, config=None):
-    redis_config = _setup_redis_config(config=config)
+def _get_redis_pool(db=None):
+    redis_config = get_redis_config()
     if redis_config is not False:
         redis_config = redis_config if redis_config is not None else dict()
-        if 'url' in redis_config:
+        if redis_config.get('url', None):
+            if db is not None:
+                raise ValueError("Can't change DB in Redis connection URL. "
+                                 "Please split the redis connection config into host and port instead of URL.")
             return ConnectionPool.from_url(redis_config['url'])
         else:
             return ConnectionPool(
@@ -60,7 +63,7 @@ def _get_redis_pool(db=None, config=None):
             )
 
 
-def _setup_redis_config(config=None):
+def get_redis_config(config=None):
     app_config = config or get_helper_config()
     elementary_config = app_config.setdefault('ELEMENTARY_FLASK', dict())
     redis_config = elementary_config.setdefault('redis', dict())
@@ -71,10 +74,10 @@ def _setup_redis_config(config=None):
             redis_config.setdefault('host', 'localhost' if debug else 'redis')
             redis_config.setdefault('port', 6379)
             redis_config.setdefault('db', 0)
-            redis_config.setdefault(
-                'key_prefix',
-                app_config.setdefault('ELEMENTARY_FLASK_APP_NAME', 'test_app').upper() + '_'
-            )
+        redis_config.setdefault(
+            'key_prefix',
+            app_config.setdefault('ELEMENTARY_FLASK_APP_NAME', 'test_app').upper() + '_'
+        )
 
     return redis_config
 
@@ -89,55 +92,72 @@ def _setup_redis_config(config=None):
 
 
 class _PotteryFactories:
-    @staticmethod
-    def RedisSet(iterable=tuple(), *, redis=None, key=''):  # noqa
-        redis = redis or redis_client()
+    def __init__(self, db=None, key_prefix=None):
+        # self.config = get_redis_config()
+        # self.url = self.config.get('url', None)
+        # if self.url and db is not None:
+        #     raise ValueError("Can't change DB in Redis connection URL. "
+        #                      "Please split the redis connection config into host and port instead of URL.")
+        self.db = db
+        self.key_prefix = key_prefix
+        # self.key_prefix = key_prefix if key_prefix is not None else self.config.get('key_prefix', '')
+
+    def _key(self, key=''):
+        if key:
+            if self.key_prefix is None:
+                self.key_prefix = get_redis_config().get('key_prefix', '')
+            return self.key_prefix + key
+        return key
+
+    def RedisSet(self, iterable=tuple(), *, redis=None, key=''):  # noqa
+        redis = redis or redis_client(db=self.db)
+        key = self._key(key)
         if key and redis.exists(key):
             return RedisSet(redis=redis, key=key)
         return RedisSet(iterable, redis=redis, key=key)
 
-    @staticmethod
-    def RedisDict(arg=tuple(), *, redis=None, key=''):  # noqa
-        redis = redis or redis_client()
+    def RedisDict(self, arg=tuple(), *, redis=None, key=''):  # noqa
+        redis = redis or redis_client(db=self.db)
+        key = self._key(key)
         if key and redis.exists(key):
             return RedisDict(redis=redis, key=key)
         return RedisDict(arg, redis=redis, key=key)
 
-    @staticmethod
-    def RedisList(iterable=tuple(), *, redis=None, key=''):  # noqa
-        redis = redis or redis_client()
+    def RedisList(self, iterable=tuple(), *, redis=None, key=''):  # noqa
+        redis = redis or redis_client(db=self.db)
+        key = self._key(key)
         if key and redis.exists(key):
             return RedisList(redis=redis, key=key)
         return RedisList(iterable, redis=redis, key=key)
 
-    @staticmethod
-    def RedisCounter(arg=tuple(), *, redis=None, key=''):  # noqa
-        redis = redis or redis_client()
+    def RedisCounter(self, arg=tuple(), *, redis=None, key=''):  # noqa
+        redis = redis or redis_client(db=self.db)
+        key = self._key(key)
         if key and redis.exists(key):
             return RedisCounter(redis=redis, key=key)
         return RedisCounter(arg, redis=redis, key=key)
 
-    @staticmethod
-    def RedisDeque(iterable=tuple(), *, redis=None, key=''):  # noqa
-        redis = redis or redis_client()
+    def RedisDeque(self, iterable=tuple(), *, redis=None, key=''):  # noqa
+        redis = redis or redis_client(db=self.db)
+        key = self._key(key)
         if key and redis.exists(key):
             return RedisDeque(redis=redis, key=key)
         return RedisDeque(iterable, redis=redis, key=key)
 
-    @staticmethod
-    def RedisSimpleQueue(arg, *, redis=None, key=''):  # noqa
-        redis = redis or redis_client()
+    def RedisSimpleQueue(self, arg, *, redis=None, key=''):  # noqa
+        redis = redis or redis_client(db=self.db)
+        key = self._key(key)
         return RedisSimpleQueue(redis=redis, key=key)
 
-    @staticmethod
-    def Redlock(*, masters=None, key='',  # noqa
+    def Redlock(self, *, masters=None, key='',  # noqa
                 raise_on_redis_errors: bool = False,
                 auto_release_time: float = _AUTO_RELEASE_TIME,
                 num_extensions: int = _NUM_EXTENSIONS,
                 context_manager_blocking: bool = True,
                 context_manager_timeout: float = -1,
                 ):
-        masters = masters or {redis_client()}
+        masters = masters or {redis_client(db=self.db)}
+        key = self._key(key)
         return Redlock(masters=masters, key=key,
                        raise_on_redis_errors=raise_on_redis_errors,
                        auto_release_time=auto_release_time,
@@ -146,29 +166,31 @@ class _PotteryFactories:
                        context_manager_timeout=context_manager_timeout
                        )
 
-    @staticmethod
-    def NextId(*,  # noqa
+    def NextId(self,  # noqa
+               *,
                key: str = 'current', masters=None,
                raise_on_redis_errors: bool = False,
                num_tries: int = _NUM_TRIES):
-        masters = masters or {redis_client()}
-        return NextId(key=key, masters=masters, raise_on_redis_errors=raise_on_redis_errors, num_tries=num_tries)
+        masters = masters or {redis_client(db=self.db)}
+        key = self._key(key)
+        return NextId(key=key, masters=masters, raise_on_redis_errors=raise_on_redis_errors,
+                      num_tries=num_tries)
 
-    @staticmethod
-    def redis_cache(  # noqa
-            *, redis=None, key='',
-            timeout=_DEFAULT_TIMEOUT):
-        redis = redis or redis_client()
+    def redis_cache(self,  # noqa
+                    *, redis=None, key='',
+                    timeout=_DEFAULT_TIMEOUT):
+        redis = redis or redis_client(db=self.db)
+        key = self._key(key)
         return redis_cache(redis=redis, key=key, timeout=timeout)
 
-    @staticmethod
-    def CachedOrderedDict(*,  # noqa
+    def CachedOrderedDict(self, *,  # noqa
                           redis=None,
                           redis_key=None,
                           dict_keys=tuple(),
                           num_tries=_CACHED_ORDERED_DICT_NUM_TRIES,
                           timeout=_DEFAULT_TIMEOUT, ):
-        redis = redis or redis_client()
+        redis = redis or redis_client(db=self.db)
+        redis_key = self._key(redis_key)
         return CachedOrderedDict(
             redis_client=redis,
             redis_key=redis_key,
@@ -176,22 +198,23 @@ class _PotteryFactories:
             num_tries=num_tries,
             timeout=timeout, )
 
-    @staticmethod
-    def BloomFilter(iterable=frozenset(), *,  # noqa
+    def BloomFilter(self, iterable=frozenset(), *,  # noqa
                     num_elements: int,
                     false_positives: float,
                     redis=None, key='',
                     ):
-        redis = redis or redis_client()
-        return BloomFilter(iterable, num_elements=num_elements, false_positives=false_positives, redis=redis, key=key)
+        redis = redis or redis_client(db=self.db)
+        key = self._key(key)
+        return BloomFilter(iterable, num_elements=num_elements, false_positives=false_positives, redis=redis,
+                           key=key)
 
-    @staticmethod
-    def HyperLogLog(  # noqa
-            iterable=frozenset(),
-            *,
-            redis=None,
-            key=''):
-        redis = redis or redis_client()
+    def HyperLogLog(self,  # noqa
+                    iterable=frozenset(),
+                    *,
+                    redis=None,
+                    key=''):
+        redis = redis or redis_client(db=self.db)
+        key = self._key(key)
         return HyperLogLog(iterable, redis=redis, key=key)
 
     ContextTimer = _ContextTimer
