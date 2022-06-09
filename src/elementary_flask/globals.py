7ec01290a74d3ce41478872af36e9a6582c86943
Khalid Grandi <kh.grandi@gmail.com>
2022-06-09 22:48:05 +0100

    cache global proxy

--- src/elementary_flask/globals.py
+++ src/elementary_flask/globals.py
@@ -5,4 +5,10 @@ def _find_app():
     return current_app.elementary_flask  # noqa
 
 
+def _find_cache():
+    return current_app.elementary_flask.cache  # noqa
+
+
 current_elementary_flask_app: "ElementaryFlask" = LocalProxy(_find_app)  # type: ignore
+current_elementary = current_elementary_flask_app
+cache = LocalProxy(_find_cache)  # type: ignore
